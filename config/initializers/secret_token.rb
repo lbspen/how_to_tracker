3d50225f6093f99dbe4e3fc2cf07ff2940a514a8
lbs <lbspen@gmail.com>
2013-05-16 11:43:57 -0700

    Initial app generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowToTracker::Application.config.secret_token = '6bb68af52a8b78c87a9fd93c7c83355d964f7c40fd9c0b9a5c179fc9742e79c23d9f5817dab84780d983445c3998fcd95c7f409f3aa269661ed8b8ffd4738c80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HowToTracker::Application.config.session_store :cookie_store, key: '_how_to_tracker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HowToTracker::Application.config.session_store :active_record_store
